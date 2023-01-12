80b6287cdcaad51f6fe5fc34b30b7548821ff04f
prathibha22 <prccherukuri@gmail.com>
2023-01-12 14:57:54 -0500

    Code to upload file to S3 bucket

--- src/main/java/rocks/zipcode/AWSs3/GetS3ObjectPresignedUrl.java
+++ src/main/java/rocks/zipcode/AWSs3/GetS3ObjectPresignedUrl.java
@@ -1,26 +1,19 @@
 package rocks.zipcode.AWSs3;
-
 import java.time.Duration;
-import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import rocks.zipcode.domain.Video;
 import rocks.zipcode.repository.VideoRepository;
-import software.amazon.awssdk.auth.credentials.EnvironmentVariableCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
-import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.*;
 import software.amazon.awssdk.services.s3.presigner.model.GetObjectPresignRequest;
 import software.amazon.awssdk.services.s3.presigner.model.PresignedGetObjectRequest;
 import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 
-import javax.persistence.EntityManager;
 
 @Service
 public class GetS3ObjectPresignedUrl {
@@ -28,32 +21,17 @@ public class GetS3ObjectPresignedUrl {
     String bucketName="tubeflix";
     @Autowired
     VideoRepository videoRepository;
-    private S3Presigner getPresigner() {
+    @Autowired
+    AWSS3ClientServices awss3ClientServices;
 
-        // Create the S3Presigner object.
-        Region region = Region.US_EAST_1;
-        return S3Presigner.builder()
-            .credentialsProvider(EnvironmentVariableCredentialsProvider.create())
-            .region(region)
-            .build();
-    }
-    private S3Client getS3Client() {
-
-        // Create the S3Client object.
-        Region region = Region.US_EAST_1;
-        return S3Client.builder()
-            .credentialsProvider(EnvironmentVariableCredentialsProvider.create())
-            .region(region)
-            .build();
-    }
     public List<String> getLstPresignedUrl(){
-        //method to generate PresignedUrl for all Objects in the bucket
+        //method to generate PresignedUrl for all Objects in the bucket and store in the repository
         ListObjectsRequest listObjects = ListObjectsRequest
             .builder()
             .bucket(bucketName)
             .build();
 
-        List<S3Object> objects = getS3Client().listObjects(listObjects).contents();
+        List<S3Object> objects = awss3ClientServices.getS3Client().listObjects(listObjects).contents();
         List<String> keys = new ArrayList<>();
         for (S3Object s3Obj: objects) {
             keys.add(getPresignedUrl(s3Obj.key()));
@@ -72,17 +50,14 @@ public class GetS3ObjectPresignedUrl {
         //method to generate PresignedUrl for key
         PresignedGetObjectRequest presignedGetObjectRequest=null;
         Region region = Region.US_EAST_1;
-        S3Presigner presigner=getPresigner();
+        S3Presigner presigner=awss3ClientServices.getPresigner();
         try {
-            ListObjectsRequest listObjects = ListObjectsRequest.builder()
-                                                               .bucket(bucketName).build();
-
             GetObjectRequest getObjectRequest = GetObjectRequest.builder().bucket(bucketName)
                                                                           .key(keyName)
                                                                           .build();
 
             GetObjectPresignRequest getObjectPresignRequest = GetObjectPresignRequest.builder()
-                                                             .signatureDuration(Duration.ofMinutes(60))
+                                                             .signatureDuration(Duration.ofDays(2))
                                                              .getObjectRequest(getObjectRequest)
                                                              .build();
 
